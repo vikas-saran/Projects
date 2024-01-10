5edda4256f7bf20524a17b8d67cf780c9da66c92
vikas saran <148774845+vikas-saran@users.noreply.github.com>
2024-01-10 13:40:37 +0530

    now

--- whether-reports/js.js
+++ whether-reports/js.js
@@ -6,19 +6,19 @@ const loding_screen=document.querySelector(".loding_screen");
 const whetherdata=document.querySelector(".whetherdata");
 
 const API_key='d1845658f92b31c64bd94f06f7188c9c';
-const current_tab=my_wheather_tab;
-current_tab.classList.add("curren_tab");
+let current_tab=my_wheather_tab;
+current_tab.classList.add("current_tab");
 
 
 function switch_screen(clicked_tab){
 
    if(current_tab!=clicked_tab){
         current_tab.classList.remove("current_tab");
-        current_tab=clicked_tab
+        current_tab=clicked_tab;
         current_tab.classList.add("current_tab");
         if(!search_bar.classList.contains("active")){
             whetherdata.classList.remove("avtive");
-            grant_access,classList.remova("active");
+            grant_access.classList.remove("active");
             search_bar.classList.add("active");
         }
         else{
@@ -30,16 +30,19 @@ function switch_screen(clicked_tab){
 }
 
 function check_access(){
-    const local_cordinates=cordinate_storage.getItem('cordinate_data');
+    const local_cordinates= sessionStorage.getItem('cordinate_data');
     if(!local_cordinates){
         grant_access.classList.add("active");
     }
     else{
-        const cordinate=JSON.parse.apply(local_cordinates);
+        const cordinate=JSON.parse(local_cordinates);
         meramosam(cordinate);
     }
 
 }
+
+const access_button=document.querySelector("#grant_access_button");
+access_button.addEventListener("click",my_location);
 
 function my_location(){
     if(navigator.geolocation){
@@ -52,7 +55,9 @@ function my_location(){
 }
 
 function showposition(position){
-    const cordinate_data = position.coords.latitude+position.coords.longitude;
+    const cordinatedata = { lat: position.coords.latitude,lon: position.coords.longitude,}
+    sessionStorage.setItem("cordinate_data",JSON.stringify(cordinatedata));
+    meramosam(cordinatedata);
 
 }
 
@@ -61,8 +66,8 @@ async function meramosam(cordinate){
     grant_access.classList.remove("active");
     loding_screen.classList.add("active");
     try{
-    const api_la = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${Lat.innerText}&lon=${Lon.innerText}&appid=${API_key}`);
-    const data = api_la.json();
+    const api_la = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`);
+    const data = await api_la.json();
     loding_screen.classList.remove("active");
     whetherdata.classList.add("active");
     publish_data(data);
@@ -74,14 +79,50 @@ async function meramosam(cordinate){
 }
 
 function publish_data(data){
-    
+    const location_name =document.querySelector("#location_name");
+    const location_flag =document.querySelector("#location_flag");
+    const sky_situation =document.querySelector(".sky_situation");
+    const skyemoticon =document.querySelector(".skyemoticon");
+    const temperature =document.querySelector(".temperature");
+    const windspeed_data =document.querySelector("#windspeed_data");
+    const humidity_data =document.querySelector("#humidity_data");
+    const clouds_data =document.querySelector("#clouds_data");
+    location_name.innerText=data?.name;
+    location_flag.src = `https://flagcdn.com/144x108/${data?.sys?.country.toLowerCase()}.png`;
+    sky_situation.innerText = data?.weather?.[0]?.description;
+    skyemoticon.src = `http://openweathermap.org/img/w/${data?.weather?.[0]?.icon}.png`;
+    temperature.innerText = `${data?.main?.temp} °C`;
+    windspeed_data.innerText = `${data?.wind?.speed} m/s`;
+    humidity_data.innerText = `${data?.main?.humidity}%`;
+    clouds_data.innerText = `${data?.clouds?.all}%`;
 }
 
+const search_input=document.querySelector("#search_bar_input");
+search_bar.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    const city= search_input.value;
+    console.log("data got in");
+    console.log(city);
+    if (city===""){
+        return;
+    }
+    else{
+        mosam(city);
+    }
+
+})
+
 async function mosam(city){
+    loding_screen.classList.add("active");
+    whetherdata.classList.remove("active");
+    grant_access.classList.remove("active");
     try{
-    const api_la = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.innerText}&appid=${API_key}`);
-    const data = api_la.json();
+    const api_la = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`);
+    const data = await api_la.json();
+    loding_screen.classList.remove("active");
+    whetherdata.classList.add("active");
     console.log("data",data);
+    publish_data(data);
     }
     catch(e){
         console.log("unable to fetch data",e);
